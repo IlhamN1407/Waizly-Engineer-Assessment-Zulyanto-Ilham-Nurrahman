fd7605973c2d7d2020c53bfec58547e37ff50e9d
Zulyanto Ilham Nurrahman <zilhamnurrahman@gmail.com>
2023-09-01 22:22:26 +0700

    update unitTest

---
+++ Implementation Test/CRUD-APP/tests/Feature/AuthTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testRequiredFieldsForRegistration()
+    {
+        $this->json('POST', 'api/register', ['Accept' => 'application/json'])
+            ->assertStatus(422)
+            ->assertJson([
+                "message" => "The given data was invalid.",
+                "errors" => [
+                    "name" => ["The name field is required."],
+                    "email" => ["The email field is required."],
+                    "password" => ["The password field is required."],
+                ]
+            ]);
+    }
+}
